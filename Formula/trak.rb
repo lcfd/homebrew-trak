1a315d9cbe3c42be2687167c79d0c25f0888441a
lcfd <9001053+fedriz@users.noreply.github.com>
2024-04-17 21:55:26 +0200

    Add click dep

--- Formula/trak.rb
+++ Formula/trak.rb
@@ -39,6 +39,11 @@ class Trak < Formula
     sha256 "72ea0c06399eb286d978fdedb6923a9eb47e1c486ce63e9b4e64fc18303972b5"
   end
 
+  resource "click" do
+    url "https://files.pythonhosted.org/packages/96/d3/f04c7bfcf5c1862a2a5b845c6b2b360488cf47af55dfa79c98f6a6bf98b5/click-8.1.7.tar.gz"
+    sha256 "ca9853ad459e787e2192211578cc907e7594e294c7ccc834310722b41b9ca6de"
+  end
+
   def install
     virtualenv_install_with_resources
   end
